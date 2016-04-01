fcd479d98f57e0a9571ec4973de79d6114887463
wangqi948570556 <wangqi@mittop.com>
2016-04-01 17:03:44 +0800

    配置日志文件路径/tmp/logs/

--- src/main/java/com/wq/produce/controller/serviceController.java
+++ src/main/java/com/wq/produce/controller/serviceController.java
@@ -1,12 +1,31 @@
 package com.wq.produce.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class serviceController {
+	//调用日志文件
+	private static Log logger = LogFactory.getLog(serviceController.class);
+	
 	@RequestMapping("/test")
-	public void a(){
-		System.out.println("hehehhehehh");
+	@ResponseBody
+	public void a(
+            @RequestParam(value = "scope", required = false) String scope,
+            HttpServletRequest request, HttpServletResponse response){
+		try {
+			int a = 3/0;
+			System.out.println("hehehhehehh");
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Find projectCategories failed.", e);
+		}
 	}
 }
