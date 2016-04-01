9bac04c30a4e56cb5d283e65ec503f7ddb35e32a
wangqi948570556 <wangqi@mittop.com>
2016-04-01 16:31:32 +0800

    修改配置文件

--- src/main/java/com/wq/produce/model/User.java
+++ src/main/java/com/wq/produce/model/User.java
@@ -1,58 +1,43 @@
 package com.wq.produce.model;
 
-import javax.persistence.Cacheable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "SOUPE_USER")
-@Cacheable
 public class User {
-    //@Cacheable(value=”accountCache”)，这个注释的意思是，当调用这个方法的时候，会从一个名叫 accountCache 的缓存中查询
-    //，如果没有，则执行实际的方法（即查询数据库），并将执行的结果存入缓存中
-    //@CacheEvict(value=”accountCache”,key=”#account.getName()”) 情况缓存 Key 是用来指定缓存的 key
-    //@CachePut 注释，这个注释可以确保方法被执行，同时方法的返回值也被记录到缓存中，实现缓存与数据库的同步更新。
-    @Column(name = "ID")
-    private Long id;
+    private String id;
 
-	@Column(name = "USERNAME", nullable = false)
-    private String username;
+    private String name;
 
-    @Column(name = "SSO_ID")
-    private String ssoId;
+    private Integer age;
 
-    @Column(name = "PASSWORD")
-    private String password;
-    public Long getId() {
-		return id;
-	}
+    private String address;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getSsoId() {
-		return ssoId;
-	}
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
 
-	public void setSsoId(String ssoId) {
-		this.ssoId = ssoId;
-	}
+    public Integer getAge() {
+        return age;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
 }
