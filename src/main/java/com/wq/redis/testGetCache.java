7a12f433e0af44db44aa7f047a4820f302a34419
wangqi948570556 <wangqi@mittop.com>
2016-05-06 16:13:13 +0800

    redis池

--- src/main/java/com/wq/redis/testGetCache.java
+++ src/main/java/com/wq/redis/testGetCache.java
@@ -1,8 +1,13 @@
 package com.wq.redis;
 
+import redis.clients.jedis.Jedis;
+
 public class testGetCache {
 
     public static void main(String[] args) {
-        
+    	Jedis Jedis = new Jedis("192.168.8.130", 6379);
+    	Jedis.isConnected();
+    	RedisCacheUtil a = new RedisCacheUtil();
+    	a.setCacheObject("wangqi", 2);
     }
 }
